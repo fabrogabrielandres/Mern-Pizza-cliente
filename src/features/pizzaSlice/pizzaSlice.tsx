7e91857951037fb011dee546ee9eac500849ebc7
Gabriel Fabro <fabrogabrielandres@gmail.com>
2021-12-08 17:43:27 -0600

    install axios and create PizzaSlice

--- src/features/pizzaSlice/pizzaSlice.tsx
+++ src/features/pizzaSlice/pizzaSlice.tsx
@@ -1,17 +1,44 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const getAllPizzasReducer = createAsyncThunk('pizza/getAllPizzas', async () => {
+	try {
+		const response = await axios.get(`/api/pizzas/getpizzas`);
+		console.log(response);
+		return response
+		
+	} catch (error) {
+		return error
+	}
+});
+
+
 
 export interface PizzaState {
-	
+	success:string,
 }
 
 const initialState: PizzaState = {
-	
+	success:""
 };
 
 export const pizzaSlice = createSlice({
 	name: 'pizza',
 	initialState,
-	reducers: {}
+	reducers: {},
+	extraReducers:{
+		[getAllPizzasReducer.pending]:(state,action)=>{
+			state.success="pending";
+		},
+		[getAllPizzasReducer.fulfilled]:(state,action)=>{
+			state.success="succes";
+		},
+		[getAllPizzasReducer.rejected]:(state,action)=>{
+			state.success="rejected";
+		}
+
+
+	}
 });
 
 // Action creators are generated for each case reducer function
